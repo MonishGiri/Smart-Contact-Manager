b4ff468867e8d2808d403b05cc4f805d05395522
MonishGiri <monishgiri46@gmail.com>
2024-05-24 20:43:58 +0530

    Responsive Navbar and Dark, Light Theme coding

--- src/main/java/com/scm/controllers/PageController.java
+++ src/main/java/com/scm/controllers/PageController.java
@@ -33,5 +33,26 @@ public class PageController {
         System.out.println("About page loading");
         return "services";
     }
+
+    // contact route
+    @GetMapping("/contact")
+    public String contactPage() {
+        System.out.println("contact page loading");
+        return "contact";
+    }
+
+    // Login route
+    @GetMapping("/login")
+    public String loginPage() {
+        System.out.println("Login page loading");
+        return "login";
+    }
+
+    // servce route
+    @GetMapping("/register")
+    public String registerPage() {
+        System.out.println("Register page loading");
+        return "register";
+    }
     
 }
